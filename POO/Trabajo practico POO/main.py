5b386698ba9e46bc4db86a7e8c57068670e52809
Joacco-Gallard0 <164371010+Joacco-Gallard0@users.noreply.github.com>
2024-11-04 12:17:19 -0300

    Add files via upload

--- POO/Trabajo practico POO/main.py
+++ POO/Trabajo practico POO/main.py
@@ -5,7 +5,7 @@ from ClsMateria import Materia, obtener_Materias
 from ClsProfesor import Profesor, obtener_profesores
 
 def conectar_db():
-    conn = sqlite3.connect('escuela.db')
+    conn = sqlite3.connect('escolar.db')
     return conn
 
 def main():
@@ -31,24 +31,24 @@ def main():
             estudiante_id = input("Id del estudiante: ")
             apellido = input("Apellido del estudiante: ")
             curso = input("Año del curso del estudiante: ")
-            Estudiante.guardar(nombre, edad, estudiante_id, apellido, curso)
+            Estudiante.guardar(conn, nombre, edad, estudiante_id, apellido, curso)
         elif opcion == '2':
             nombre = input("Nombre del profesor: ")
             apellido = input("Apellido del profesor: ")
             profesor_id = input ("Id del profesor:")
             curso = input("Año del curso del profesor: ")
             materia = input("Materia que da el profesor: ")
-            Profesor.guardar(nombre, apellido, profesor_id, curso, materia)
+            Profesor.guardar(conn, nombre, apellido, profesor_id, curso, materia)
         elif opcion == '3':
             nombre = input("Nombre del esudiante: ")
             materia_id = input("Id de materia: ")
             curso = input("Año del curso: ")
-            Materia.guardar(nombre, materia_id, curso)
+            Materia.guardar(conn, nombre, materia_id, curso)
         elif opcion == '4':
             estudiante_id = input("Id del estudiante: ")
             materia_id = input("Id de materia: ")
             nota = input ("Ingrese la nota del estudiante")
-            Calificacion.guardar(estudiante_id, materia_id, nota)
+            Calificacion.guardar(conn, estudiante_id, materia_id, nota)
         elif opcion == '5':
             obtener_estudiantes(conn)
         elif opcion == '6':
